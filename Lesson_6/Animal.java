9e93914936de22094a6787c1805c4cb64b2e168c
EvgeniaMSH <evgeniash@yandex.ru>
2019-04-08 21:58:32 +0300

    Д/З_6

---
+++ Lesson_6/Animal.java
@@ -0,0 +1,9 @@
+package Lesson_6;
+
+public private class Animal {
+    public private boolean run(double run);
+
+    public private boolean jump(double jump);
+
+    public private boolean swim(double swim);
+}
